a9ef110b819ece0c2569565f3a19e40aca341238
Rodolfo De Nadai <rdenadai@gmail.com>
2025-09-10 23:03:22 -0300

    fix: Python 3.9 dont support staticmethod call inside class

--- src/support/utils.py
+++ src/support/utils.py
@@ -11,11 +11,13 @@ STRUCTURAL_TYPES = (Sequence, Iterator, Mapping, _Set, ItemsView)
 
 
 def materialize(func: Callable[..., Any]) -> pipe:
+    """Decorator to ensure the function's output is materialized."""
     func._materialize = True  # type: ignore
     return pipe(func)
 
 
 def from_generator(data: Any) -> Any:
+    """Materializes data from a generator if needed."""
     # data is a generator that could contain one single value or multiple values
     if isinstance(data, Generator):
         data = list(data)
@@ -25,7 +27,9 @@ def from_generator(data: Any) -> Any:
     return data
 
 
-def create_converter(target_type: Type[T], special_logic: Optional[Callable[[Any], T]] = None) -> Callable[[Any], T]:
+def create_converter_to_value(
+    target_type: Type[T], special_logic: Optional[Callable[[Any], T]] = None
+) -> Callable[[Any], T]:
     """Factory to create 'to_value' conversion methods."""
 
     @materialize
@@ -43,7 +47,19 @@ def create_converter(target_type: Type[T], special_logic: Optional[Callable[[Any
     return to_value
 
 
+def string_to_value() -> Callable[[Any], str]:
+    """Factory to create 'to_value' conversion methods for strings."""
+
+    @materialize
+    def to_value(data: Any) -> str:
+        return str(from_generator(data))
+
+    return to_value
+
+
 def proxy_str_method(method_name: str, sep: Optional[str] = None) -> Callable[..., Any]:
+    """Factory to create string method proxies that can handle both single strings and collections of strings."""
+
     @materialize
     def method(data: Any, *args: Any, **kwargs: Any) -> Any:
         data = from_generator(data)
